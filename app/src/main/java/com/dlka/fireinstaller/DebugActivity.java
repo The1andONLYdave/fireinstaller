74e37645e7ee86e097e24ec8dbf6e807157aac4c
David-Lee Kulsch <The1andONLYdave@gmail.com>
2015-10-13 08:39:44 +0200

    drawer testing
    http://mikepenz.github.io/MaterialDrawer/

--- app/src/main/java/com/dlka/fireinstaller/DebugActivity.java
+++ app/src/main/java/com/dlka/fireinstaller/DebugActivity.java
@@ -10,6 +10,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 import com.nononsenseapps.filepicker.FilePickerActivity;
 
 import java.io.DataInputStream;
@@ -25,6 +31,30 @@ public class DebugActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_debug);
+        //new DrawerBuilder().withActivity(this).build();
+//if you want to update the items at a later time it is recommended to keep it in a variable
+        PrimaryDrawerItem item1 = new PrimaryDrawerItem().withName("homescreen");
+        SecondaryDrawerItem item2 = new SecondaryDrawerItem().withName("settings");
+
+//create the drawer and remember the `Drawer` result object
+        Drawer result = new DrawerBuilder()
+                .withActivity(this)
+                .addDrawerItems(
+                        item1,
+                        new DividerDrawerItem(),
+                        item2,
+                        new SecondaryDrawerItem().withName("settings")
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        // do something with the clicked item :D
+                        return true;
+                    }
+                })
+                .build();
+
+        result.openDrawer();
 
         final Button bf = (Button) findViewById(R.id.button);
         final Button bs = (Button) findViewById(R.id.button2);
